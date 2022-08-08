8ad2fd40bf09e25637cf313ad41dbd342ef17c0f
ndcalabrese <ndcalabrese@gmail.com>
2022-08-07 20:44:38 -0400

    Upvoting and downvoting now shows the votecount

--- src/main/resources/static/js/vote.js
+++ src/main/resources/static/js/vote.js
@@ -1,36 +1,40 @@
 $(document).ready(function() {
     $('.vote-up').submit(function(e) {
-        // e.preventDefault();
+        e.preventDefault();
 
         const postId = $(this).data('id');
         $.ajax({
             type: 'POST',
             contentType: 'application/json',
-            data: JSON.stringify( { "voteType": "UPVOTE", "postId": 1}) ,
+            data: JSON.stringify( { "voteType": "UPVOTE", "postId": postId}) ,
             url: '/api/posts/' + postId + '/upvote',
             success: function(data) {
                 console.log('voted up!');
+                setInterval('location.reload()', 200);
             },
             error: function(err) {
                 console.log(err.messsage);
+
             }
         });
     });
     $('.vote-down').submit(function(e) {
-        // e.preventDefault();
+        e.preventDefault();
 
         const postId = $(this).data('id');
         $.ajax({
             type: 'POST',
             contentType: 'application/json',
-            data: JSON.stringify( { "voteType": "DOWNVOTE", "postId": 1}) ,
+            data: JSON.stringify( { "voteType": "DOWNVOTE", "postId": postId}) ,
             url: '/api/posts/' + postId + '/upvote',
             success: function(data) {
                 console.log('voted down!');
+                setInterval('location.reload()', 200);
             },
             error: function(err) {
                 console.log(err.messsage);
             }
         });
     });
+
 });
